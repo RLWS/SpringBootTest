a3e1666490a036600615c574ffaba3d559c22f95
RLWS靜 <1600510083@qq.com>
2019-02-20 20:43:32 +0800

    添加了一些yml配置;移除了数据源配置

--- src/main/java/com/rlws/spring/boot/springboot/SpringBootTestApplication.java
+++ src/main/java/com/rlws/spring/boot/springboot/SpringBootTestApplication.java
@@ -1,13 +1,16 @@
 package com.rlws.spring.boot.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringBootTestApplication {
 
     @RequestMapping(value = "",method = RequestMethod.GET)
