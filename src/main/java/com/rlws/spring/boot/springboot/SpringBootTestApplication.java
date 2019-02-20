325efff965cd2d95662bb484163a0aaf54f078a4
RLWS靜 <1600510083@qq.com>
2019-02-20 15:39:31 +0800

    第一个springboot运行起来了,不过什么都没有添加

---
+++ src/main/java/com/rlws/spring/boot/springboot/SpringBootTestApplication.java
@@ -0,0 +1,22 @@
+package com.rlws.spring.boot.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringBootTestApplication {
+
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public String Test(){
+        return "helle spring boot";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestApplication.class, args);
+    }
+
+}
